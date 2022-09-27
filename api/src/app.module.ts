83179fb543eda48ecb1bcc98337b99ffc77597cc
CindyGraffin <graffincindy@gmail.com>
2022-09-27 11:49:57 +0200

    ajout module forroot de typeorm et configservice

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: parseInt(configService.get('DATABASE_PORT')),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        entities: ['dist/**/*.model.js'],
+        // TODO: passer le synchronize en false en mode production
+        synchronize: true
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
