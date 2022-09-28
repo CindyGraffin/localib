c62db7bdca71653a6049af928c9991bd418bdbff
CindyGraffin <graffincindy@gmail.com>
2022-09-28 11:42:16 +0200

    feat(UI): composant et directives button

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './shared/button/button.component';
-import { ButtonDirective } from './shared/button/button.directive';
+import { BlueButtonDirective } from './shared/button/directives/blue-button.directive';
+import { IconComponent } from './shared/icon/icon.component';
+import { ButtonShadowDirective } from './shared/button/directives/button-shadow.directive';
+import { BlueLightButtonDirective } from './shared/button/directives/blue-light-button.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    ButtonDirective
+    BlueButtonDirective,
+    IconComponent,
+    ButtonShadowDirective,
+    BlueLightButtonDirective
   ],
   imports: [
     BrowserModule,
--- front/src/app/shared/button/button.component.ts
+++ front/src/app/shared/button/button.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {}
-
+  @Input() buttonText!: string;
+  @Input() buttonColor!: string;
 }
