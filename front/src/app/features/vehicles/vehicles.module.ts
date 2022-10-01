ab254a41969f344685bac3dfe7d7939947c21fe5
CindyGraffin <graffincindy@gmail.com>
2022-10-01 19:07:45 +0200

    feat(customers): bouton annules les modifications

--- front/src/app/features/vehicles/vehicles.module.ts
+++ front/src/app/features/vehicles/vehicles.module.ts
@@ -5,11 +5,13 @@ import { VehiclesRoutingModule } from './vehicles-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddVehicleFormComponent } from './add-vehicle-form/add-vehicle-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VehicleCardComponent } from './vehicle-card/vehicle-card.component';
 
 @NgModule({
   declarations: [
     VehiclesComponent,
-    AddVehicleFormComponent
+    AddVehicleFormComponent,
+    VehicleCardComponent
   ],
   imports: [
     CommonModule,
